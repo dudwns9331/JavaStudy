518fe8ce6e7a7f2cd74a8544901370d1fefe4014
dudwns9331 <dudwns1045@naver.com>
2020-11-11 20:55:43 +0900

    2020/11/11

--- JavaStudy/Moongorithm/src/homework4/Dijkstra_path.java
+++ JavaStudy/Moongorithm/src/homework4/Dijkstra_path.java
@@ -1,7 +1,16 @@
 package homework4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Dijkstra_path {
+
+    static final int INF = 999;
+
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
 
         int W[][] = {
                 {0, 1, 3, 999, 999},
@@ -11,28 +20,59 @@ public class Dijkstra_path {
                 {999, 999, 2, 5, 0}
         };
 
-        dijkstra(5, W);
+        System.out.print("몇 개의 노드를 생성 하시겠습니까? ");
+        int n = input.nextInt();
+
+        int R_W[][] = new int [n][n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j)
+                    R_W[i][j] = 0;
+                else if(R_W[i][j] == R_W[j][i]) {
+                    R_W[i][j] = (random.nextInt(10) +1 < 4 ? INF : random.nextInt(10) + 1);
+                    R_W[j][i] = R_W[i][j];
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++)
+                System.out.print(R_W[i][j] + "\t");
+            System.out.println();
+        }
+
+        int weight[];
+
+        weight = dijkstra(5, R_W);
+
+        for(int i = 0; i < W.length -1; i++) {
+            System.out.println("V1에서" + "V" + (i+2) + "까지의 최단 경로 비용 : " + weight[i]);
+        }
     }
 
-    public static void dijkstra(int n, int W[][]) {
+    public static int [] dijkstra(int n, int W[][]) {
         int i, vnear;
         int min;
-        int edge;
+        int cost;
 
         int touch[] = new int[n];
         int length[] = new int[n];
 
+        int F[] = new int[n-1];
+
         touch[0] = 0;
-        length[0] = 0;
+        length[0] = -1;
         vnear = 0;
 
+
         for (i = 1; i < n; i++) {
-            touch[i] = 1;
-            length[i] = W[1][i];
+            touch[i] = 0;
+            length[i] = W[0][i];
         }
 
-        for (int k = 0; k < n; k++) {
-            min = Integer.MAX_VALUE;
+        for (int k = 0; k < n-1; k++) {
+            min = 999;
             for (i = 1; i < n; i++) {
                 if (length[i] >= 0 && length[i] < min) {
                     min = length[i];
@@ -40,16 +80,16 @@ public class Dijkstra_path {
                 }
             }
 
-            edge = touch[vnear];
-            System.out.println(edge);
+            cost = length[vnear];
+            F[vnear -1] = cost;
 
-            for (i = 1; i < n; i++) {
+            for (i = 1; i < n; i++)
                 if (length[vnear] + W[vnear][i] < length[i]) {
                     length[i] = length[vnear] + W[vnear][i];
                     touch[i] = vnear;
                 }
-                length[vnear] = -1;
-            }
+            length[vnear] = -1;
         }
+        return F;
     }
 }
--- JavaStudy/Moongorithm/src/homework4/Shortest_Path_DP.java
+++ JavaStudy/Moongorithm/src/homework4/Shortest_Path_DP.java
@@ -1,5 +1,8 @@
 package homework4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Shortest_Path_DP {
 
     static final int INF = 9999;
@@ -7,28 +10,71 @@ public class Shortest_Path_DP {
     static int D[][];
     static int P[][];
 
+
     public static void main(String[] args) {
-        int W[][] = {
-                {0, 1, INF, 1, 5},
-                {9, 0, 3, 2, INF},
-                {INF, INF, 0, 4, INF},
-                {INF, INF, 2, 0, 3},
-                {3, INF, INF, INF, 0}};
 
-        D = new int[5][5];
-        P = new int[5][5];
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
 
-        floyd(5, W);
+//        int W[][] = {
+//                {0, 1, INF, 1, 5},
+//                {9, 0, 3, 2, INF},
+//                {INF, INF, 0, 4, INF},
+//                {INF, INF, 2, 0, 3},
+//                {3, INF, INF, INF, 0}};
 
-        print_m(5, D);
 
-        System.out.println();
-        System.out.println();
+        System.out.print("몇 개의 노드를 생성 하시겠습니까? ");
+        int n = input.nextInt();
 
-        print_m(5, P);
+        int R_W[][] = new int[n][n];
 
-        System.out.print("path(5, 3)의 경로 : ");
-        path(4, 2);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                R_W[i][j] = ((random.nextInt(10) + 1) < 3) ? INF : random.nextInt(10) + 1;
+                if (i == j) {
+                    R_W[i][j] = 0;
+                }
+            }
+        }
+
+//        print_m(n, R_W);
+
+        D = new int[n][n];
+        P = new int[n][n];
+
+//        floyd(5, W);
+
+        floyd(n, R_W);
+
+//        print_m(5, D);
+
+//        System.out.println();
+//        System.out.println();
+
+//        print_m(5, P);
+
+//        System.out.print("path(5, 3)의 경로 : ");
+//        path(4, 2);
+
+
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (P[i][j] < 0) {
+//                    continue;
+//                } else {
+//                    System.out.print("path(" + (i + 1) + "," + (j + 1) + ") : ");
+//                    System.out.print("v" + (i + 1) + "->");
+//                    path(i, j);
+//                    System.out.print("v" + (j + 1));
+//                    System.out.print(" 필요한 비용 : " + D[i][j]);
+//                    System.out.println();
+//                }
+//            }
+//        }
+
+//        print_m(n, R_W);
     }
 
     public static void floyd(int n, int W[][]) {
@@ -39,22 +85,28 @@ public class Shortest_Path_DP {
             }
         }
 
+
         D = W;
+
+//        for(i = 0; i < n; i++) {
+//            for(j = 0; j < n; j++)
+//                D[i][j] = W[i][j];
+//        }
 
         for (k = 0; k < n; k++)
             for (i = 0; i < n; i++)
                 for (j = 0; j < n; j++)
                     if ((D[i][k] + D[k][j]) < D[i][j]) {
-                            P[i][j] = k;
-                            D[i][j] = D[i][k] + D[k][j];
-                        }
+                        P[i][j] = k;
+                        D[i][j] = D[i][k] + D[k][j];
+                    }
 
     }
 
     public static void path(int q, int r) {
         if (P[q][r] != -1) {
             path(q, P[q][r]);
-            System.out.print("v" + (P[q][r] + 1) + "\t");
+            System.out.print("v" + (P[q][r] + 1) + "->");
             path(P[q][r], r);
         }
     }
@@ -63,7 +115,10 @@ public class Shortest_Path_DP {
     public static void print_m(int n, int A[][]) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(A[i][j] + "\t");
+                if (A[i][j] == INF)
+                    System.out.print("INF" + "\t");
+                else
+                    System.out.print(A[i][j] + "\t");
             }
             System.out.println();
         }
